86cda6c06cc794dab1f4b4f78ecc2981385523f3
Jessica Tingley <jtingley2003@gmail.com>
2023-02-16 20:50:58 -0500

    Wrote the category header

--- Category.h
+++ Category.h
@@ -1,4 +1,17 @@
+#include <iostream>
+#include <vector>
+
 #ifndef CSC212_GRADEBOOK_CATEGORY_H
 #define CSC212_GRADEBOOK_CATEGORY_H
-
+class Category{
+private:
+    std::string category;
+    std::string command;
+    std::vector<std::vector<int>> grades;
+    int DetermineCategory(std::string category);
+public:
+    Category(std::string category, std::string command, std::vector<std::vector<int>> grades);
+    std::string AllGrades(std::string category, std::vector<std::vector<int>> grades);
+    double Total(std::string category, std::vector<std::vector<int>> grades);
+};
 #endif //CSC212_GRADEBOOK_CATEGORY_H
