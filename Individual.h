065cce0d9ad2057c5355b1a250b335821a7fe720
BrynneWright <90731631+BrynneWright@users.noreply.github.com>
2023-02-22 13:30:43 -0500

    Update Individual.h

--- Individual.h
+++ Individual.h
@@ -2,10 +2,19 @@
 #ifndef CSC212_GRADEBOOK_INDIVIDUAL_H
 #define CSC212_GRADEBOOK_INDIVIDUAL_H
 
+#include <iostream>
+#include <vector>
 
 class Individual {
+private:
+    std::string category;
+    std::string deliverable;
+    std::vector<std::vector<int>> grades;
+    int DetermineCategory(std::string category);
 
+public:
+    Individual(std::string category, std::string deliverable, std::vector<std::vector<int>> grades);
+    int GetGrade(std::string category, std::string deliverable, std::vector<std::vector<int>> grades);
 };
-
 
 #endif //CSC212_GRADEBOOK_INDIVIDUAL_H
