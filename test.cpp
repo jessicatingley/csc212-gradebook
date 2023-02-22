ee920f297b2b26319523cc7a3da826d616b6169a
Jessica Tingley <jtingley2003@gmail.com>
2023-02-22 13:18:55 -0500

    Updated assignment_names vector

--- test.cpp
+++ test.cpp
@@ -1,30 +1,39 @@
+#include "Individual.h"
+#include "Category.h"
+#include "Course.h"
+#include <iostream>
+#include <sstream>
+#include <vector>
+
 // Test Individual Class
-void TestIndividualAssignment{
+void *TestIndividualAssignment{
 
 };
 
-void TestIndividualGrade{
+void *TestIndividualGrade{
 
 };
 
 // Test Category Class
-void TestCategoryAllGrades{
-
+void *TestCategoryAllGrades{
+    Category test_category()
+    std::vector<std::vector<double>> grades = {{0 0 0 0 0 0 0 0 0 0 0 0 0 0}, {0 0 0 0}, {0 0}, {0}};
+    std::string result =
 };
 
-void TestCategoryTotal{
+void *TestCategoryTotal{
 
 };
 
 // Test Course Class
-void TestCourseAllGrades{
+void *TestCourseAllGrades{
 
 };
 
-void TestCourseTotals{
+void *TestCourseTotals{
 
 };
 
-void TestCourseOverall{
+void *TestCourseOverall{
 
 };
