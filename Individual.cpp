0ebfa11f880d7b1df1f90a3d653a75640bc0efbc
asampalis <98431552+asampalis@users.noreply.github.com>
2023-02-22 16:13:01 -0500

    Update Individual.cpp

--- Individual.cpp
+++ Individual.cpp
@@ -1,34 +1,43 @@
 #include "Individual.h"
+#include "Individual.h"
 #include <iostream>
 
 Individual::Individual(std::string category, std::string command, std::vector<std::vector<int>> grades, std::vector<std::vector<std::string>> assignment_names)
-        : category(category), command(command), grades(grades), assignment_names(assignment_names){}
+    : category(category), command(command), grades(grades), assignment_names(assignment_names) {}
 
-int Individual::DetermineCategory(std::string category) {
-//    enum category_type{
-//        LABS,
-//        ASSIGNMENTS,
-//        PROJECTS,
-//        EXAM
-//    };
+int Individual::DetermineCategory(std::string category)
+{
+    //    enum category_type{
+    //        LABS,
+    //        ASSIGNMENTS,
+    //        PROJECTS,
+    //        EXAM
+    //    };
 
-    if(category == "LABS") return 0;
-    if(category == "ASSIGNMENTS") return 1;
-    if(category == "PROJECTS") return 2;
-    if(category == "EXAM") return 3;
-
+    if (category == "LABS")
+        return 0;
+    if (category == "ASSIGNMENTS")
+        return 1;
+    if (category == "PROJECTS")
+        return 2;
+    if (category == "EXAM")
+        return 3;
 }
 
 // Get index of individual deliverable (command) from assignment_names
-int Individual::GetIndex(std::string category, std::string command, std::vector<std::vector<std::string>> assignment_names) {
-    for(int i = 0; i < grades[DetermineCategory(category)].length(); i++) {
-        if(command == assignment_names[DetermineCategory(category)][i]) {
+int Individual::GetIndex(std::string category, std::string command, std::vector<std::vector<std::string>> assignment_names)
+{
+    for (int i = 0; i < size(grades[DetermineCategory(category)]); i++)
+    {
+        if (command == assignment_names[DetermineCategory(category)][i])
+        {
             return i;
         }
     }
 }
 
 // Get grade of individual deliverable (command) from grades
-int Individual::GetGrade(std::string category, std::string command, std::vector<std::vector<int>> grades) {
-     return grades[DetermineCategory(category)][GetIndex(category, command, assignment_names)]
+int Individual::GetGrade(std::string category, std::string command, std::vector<std::vector<int>> grades)
+{
+    return grades[DetermineCategory(category)][GetIndex(category, command, assignment_names)];
 }
