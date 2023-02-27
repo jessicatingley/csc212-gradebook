3e98709702d4c142c9a0c15e380538594d57d003
asampalis <98431552+asampalis@users.noreply.github.com>
2023-02-27 16:18:12 -0500

    Update Course.cpp

--- Course.cpp
+++ Course.cpp
@@ -24,6 +24,23 @@ std::string Course::TOTAL(std::string category, std::vector<std::vector<double>>
             all_grades += std::to_string(grade) + " ";
         }
         return all_grades;
+        if (i == 0)
+        {
+            // have to drop two of the lowest grades
+            std::cout << "Labs: " << all_grades << std::endl;
+        }
+        if (i == 1)
+        {
+            std::cout << "Assignments: " << all_grades << std::endl;
+        }
+        if (i == 2)
+        {
+            std::cout << "Projects: " << all_grades << std::endl;
+        }
+        if (i == 3)
+        {
+            std::cout << "Exam: " << all_grades << std::endl;
+        }
         all_grades.clear();
     }
 }
@@ -38,23 +55,44 @@ std::string Course::CATEGORY_TOTAL(std::string category, std::vector<std::vector
         double total = 0;
         for (const auto grade : grades[i])
         {
-            total += grade;
-        }
-        if (i == 0)
-        {
-            std::cout << "Lab total: " << total << std::endl;
-        }
-        if (i == 1)
-        {
-            std::cout << "Assignment total: " << total << std::endl;
-        }
-        if (i == 2)
-        {
-            std::cout << "Project total: " << total << std::endl;
-        }
-        if (i == 3)
-        {
-            std::cout << "Exam total: " << total << std::endl;
+            if (grade != 999)
+            {
+                total += grade;
+            }
+
+            if (i == 0)
+            {
+                // grade[0]
+                if (grades.size() == 12)
+                {
+                    total = 0;
+                    //  drop 2 lowest grades
+                    // sort in descending
+                    std::sort(grades.begin(), grades.end(), std::greater<int>());
+                    // add up first 10 indicies (ignore last lowest)
+                    for (int a = 0; a++; a < 10)
+                    {
+                        total += grades[a];
+                    }
+                }
+
+                else
+                {
+                    std::cout << "Lab total: " << total << std::endl;
+                }
+            }
+            if (i == 1)
+            {
+                std::cout << "Assignment total: " << total << std::endl;
+            }
+            if (i == 2)
+            {
+                std::cout << "Project total: " << total << std::endl;
+            }
+            if (i == 3)
+            {
+                std::cout << "Exam total: " << total << std::endl;
+            }
         }
     }
 }
@@ -65,11 +103,18 @@ std::string Course::CATEGORY_TOTAL(std::string category, std::vector<std::vector
 double Course::COURSE_TOTAL(std::string category, std::vector<std::vector<double>> grades)
 {
     double total = 0;
-    int i = 0;
-    for (const auto grade : grades[i])
+
+    // rows
+    for (const auto category : grades)
     {
-        total += grade;
-        i++;
+        // columns
+        for (const auto grade : category)
+        {
+            if (total != 999)
+            {
+                total += grade;
+            }
+        }
     }
     return total;
 }
